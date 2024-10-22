c4f3f4b13c54c164e0d7cc704fc080e5eee6fac2
livweinst5 <osweinst@ucsc.edu>
2024-10-21 19:06:55 -0700

    update5

--- lab5/js/lab.js
+++ lab5/js/lab.js
@@ -1,6 +1,24 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Data Types and Variables
+// Author: Liv Weinstein <osweinst@ucsc.edu
+// Date: 21 October
+
+// Constants
+
+// Define Variables
+make = "Prius";
+model = "Toyata Prius Three";
+color = "Grey";
+year = 2016;
+
+// calculate
+age = 2019 - year;
+
+// output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + make + "<br>");
+document.writeln("Color: " + make + "<br>");
+document.writeln("Year: " + make + "<br>");
+document.writeln("Age: " + make + " years<br>");
 
 // Constants
 
