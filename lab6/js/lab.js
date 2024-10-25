25bf8c525a591045fe67763383686feee2b13a96
livweinst5 <osweinst@ucsc.edu>
2024-10-24 20:52:02 -0700

    update lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,18 +1,37 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Lab 6: Arrays and Objects
+// Author: Liv Weinstein <osweinst@ucsc.edu>
+// Date: 24 october
 
-// Constants
+// Define Variables
+myTransport = ["Toyata Prius Three, Train, Bike"];
 
-// Functions
+// create an object for my main ride
+myMainRide = { 
+    make: "Prius",
+    model: "Toyata Prius Three",
+    color: "Grey",
+    year: 2016,
+    // we can define a function within our object (called a method)
+    // that uses the value of year above (using this.year)
+    age : function () {
+      return 2023 - this.year;
+    }
+  }
 
-// this is an example function and this comment tells what it doees and what parameters are passed to it.
-function myFunction(param1, param2) {
+// output
+document.writeln("Kinds of transportation I use: ", myTransport,"</br>");
+// this little trick allows us to write an object to the document
+document.writeln("My Main Ride: <pre>",
+     JSON.stringify(myMainRide, null, '\t'), "</pre>");
+//Constanta
+
+//Functions
+function myFunction(param1,param2) {
   // some code here
   // return results;
 }
 
-function main() {
+function Main() {
   console.log("Main function started.");
   // the code that makes everything happen
 }
